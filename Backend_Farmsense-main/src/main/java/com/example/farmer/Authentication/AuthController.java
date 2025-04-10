48d59e58926558eab0014ce725f35c6a52515402
Lahiru2000 <it22298430@my.sliit.lk>
2025-04-10 22:10:50 +0530

    backend

---
+++ Backend_Farmsense-main/src/main/java/com/example/farmer/Authentication/AuthController.java
@@ -0,0 +1,40 @@
+package com.example.farmer.Authentication;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody User user) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+        );
+        final UserDetails userDetails = userService.loadUserByUsername(user.getUsername());
+        final String token = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(token);
+    }
+}
